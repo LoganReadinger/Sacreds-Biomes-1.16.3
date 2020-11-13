4fb5f21f832e5e1b6cae9481205486b0b3f4236a
Logan <loganreadinger1@gmail.com>
2020-11-13 02:49:06 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sacreds-Biomes-1.16.3
+ A Perlin Noise based noise generation option for surface terrain along with a custom portal to take you there.
---
+++ src/main/java/com/sacredninja/sacredsbiomes/SacredsBiomes.java
@@ -0,0 +1,55 @@
+package com.sacredninja.sacredsbiomes;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(SacredsBiomes.MOD_ID)
+@Mod.EventBusSubscriber(modid = SacredsBiomes.MOD_ID, bus = Bus.MOD)
+public class SacredsBiomes{
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "sacredsbiomes";
+    public static SacredsBiomes instance;
+    
+    public SacredsBiomes() {
+    	final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::setup);
+        modEventBus.addListener(this::doClientStuff);
+
+    	
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        
+        //BiomeInit.BIOMES.register(modEventBus);
+        
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public static void onRegisterBiomes(final RegistryEvent.Register<Biome> event) {
+    	//BiomeInit.registerBiomes();
+    }
+    
+    private void setup(final FMLCommonSetupEvent event){	
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {
+    }
+}
